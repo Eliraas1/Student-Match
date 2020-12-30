470810311c7e54b415a8043d7fe74d318deffeb9
Eliraas1 <eliraas1@ac.sce.ac.il>
2020-12-30 18:08:16 +0200

    Profile and css

--- student_social/student_admin/urls.py
+++ student_social/student_admin/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('thanks/',views.ThankPage.as_view(), name='thanks'),
     path('posts/',include('posts.urls',namespace='posts')),
     path('groups/',include('groups.urls',namespace= 'groups')),
+
     ]
